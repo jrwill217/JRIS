90dc6792ca99278726ebed330aeecc8900fa0257
jrwill217 <williamshenjr@gmail.com>
2025-01-08 20:34:24 +0800

    react学习

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+function MyComponent() { 
+  return <h1>你好</h1>
+}
+
+
+
+function App() {
+
+  return (
+    <div><MyComponent /></div>
+  )
+}
+
+export default App
---
+++ my-react-app/src/ChildComponent.jsx
@@ -0,0 +1,4 @@
+function ChildComponent(){
+    return<h2>我是自组子组件</h2>
+}
+export default ChildComponent;
